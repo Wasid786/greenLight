bde8206616bc848a3cbc32f0f13c7ac07e2b4e46
Wasid786 <114327679+Wasid786@users.noreply.github.com>
2025-02-25 00:04:38 +0530

    3.3 complete

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,18 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	// fmt.Fprintf(w, "Status: available")
-	// fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	// fmt.Fprintf(w, "version: %s \n", version)
 
-	js := `{"status":"available", "environment":%q, "version": %q}`
-	js = fmt.Sprintf(js, app.config.env, version)
+	data := map[string]string{
+		"status:":     "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(js))
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encounterd a problem and could not process your request", http.StatusInternalServerError)
+
+	}
+
 }
