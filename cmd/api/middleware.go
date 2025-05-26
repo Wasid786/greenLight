5111f957dc46b7a4dc658226c6f0a637be164232
Wasid786 <114327679+Wasid786@users.noreply.github.com>
2025-05-26 15:36:37 +0530

    chapter 19 complete

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"errors"
+	"expvar"
 	"fmt"
 	"net"
 	"net/http"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
 
+	"github.com/felixge/httpsnoop"
 	"golang.org/x/time/rate"
 	"greenlight.Wasid786/internal/data"
 	"greenlight.Wasid786/internal/validator"
@@ -187,5 +190,25 @@ func (app *application) enableCORS(next http.Handler) http.Handler {
 			}
 		}
 		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) metrics(next http.Handler) http.Handler {
+	totalRequestRecieved := expvar.NewInt("total_requests_recieved")
+	totalResponsesSent := expvar.NewInt("total_responses_sent")
+	totalProcessingTimeMicroseconds := expvar.NewInt("total_processing_time_μs")
+
+	totalResonsesSentByStatus := expvar.NewMap("total_resonses_sent_by_status")
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		totalRequestRecieved.Add(1)
+		metrics := httpsnoop.CaptureMetrics(next, w, r)
+		totalResponsesSent.Add(1)
+
+		totalProcessingTimeMicroseconds.Add(metrics.Duration.Microseconds())
+
+		totalResonsesSentByStatus.Add(strconv.Itoa(metrics.Code), 1)
+
 	})
 }
